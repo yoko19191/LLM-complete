57b992e8e7933c7fcbde38be059bd3ee4bb54a9b
angelkawaii2 <box690126749@gmail.com>
2024-07-02 13:06:28 +0800

    update

---
+++ streamlit_app/01_chat_bot/fastapi_back.py
@@ -0,0 +1,33 @@
+# cite : https://github.com/xccds/Build_LLM_APP/blob/main/01_ChatGPT_Clone/fastapi_back.py
+# uvicorn fastapi_back:app --reload 
+
+from fastapi import FastAPI
+from openai import OpenAI
+from pydantic import BaseModel
+
+client = OpenAI()
+app = FastAPI()
+
+
+class Item(BaseModel):
+    role: str
+    content: str
+
+class ReqestIntputs(BaseModel):
+    model: str
+    messages: list[Item]
+
+def openai_response(model,messages):
+    chat_response = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+    reply = chat_response.choices[0].message.content
+    return reply
+
+@app.post("/openai")
+async  def openai_AI(reqest_intputs:ReqestIntputs):
+    reply = openai_response(reqest_intputs.model, reqest_intputs.messages)
+    return reply
+
+
