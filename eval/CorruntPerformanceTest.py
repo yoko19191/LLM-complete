ebb39b473715d4b59269f0f611f1abef14e8dc1c
angelkawaii2 <box690126749@gmail.com>
2024-08-25 10:23:10 +0800

    refactor project folder

---
+++ eval/CorruntPerformanceTest.py
@@ -0,0 +1,10 @@
+"""
+ConcurrenceInferenceTest.py
+test items: 
+1. total time(s)
+2. Throughput(requests/s)
+3. Average latency(ms)
+4. Average latency per token(ms)
+5. Average latency per output token(ms)
+6. Throughput(tokens/s)
+""" 
---
+++ eval/benchmark.sh
@@ -0,0 +1,5 @@
+python inference_bench.py 1 4
+python inference_bench.py 2 8
+python inference_bench.py 4 16
+python inference_bench.py 8 32
+python inference_bench.py 16 64
